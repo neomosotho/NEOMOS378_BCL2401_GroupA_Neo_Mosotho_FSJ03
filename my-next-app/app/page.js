2b5c10b101942464f9ab64f2d8e98351b847973d
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-09-10 14:44:01 +0200

    "Added NavBar component and imported it into page.js"

--- my-next-app/app/page.js
+++ my-next-app/app/page.js
@@ -1,10 +1,12 @@
 // app/page.js
 import ProductGrid from '../components/ProductGrid'
+import NavBar from '@/components/NavBar'
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">Our Products</h1>
+      <NavBar/>
       <ProductGrid />
     </div>
   )
