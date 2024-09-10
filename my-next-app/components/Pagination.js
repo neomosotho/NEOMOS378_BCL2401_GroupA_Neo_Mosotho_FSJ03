89fcfed0adb088e647d5c2ac094494a37824e705
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-09-10 15:23:21 +0200

    "Added Pagination component with function signature and totalPages calculation"

---
+++ my-next-app/components/Pagination.js
@@ -0,0 +1,7 @@
+export default function Pagination({ currentPage, totalProducts, productsPerPage, onPageChange }) {
+    const totalPages = Math.ceil(totalProducts / productsPerPage)
+
+    return (
+        
+    )
+}
