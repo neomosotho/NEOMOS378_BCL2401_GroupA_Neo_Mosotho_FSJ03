4619f1818840781467f057c68c654c48315a575e
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-09-12 09:47:53 +0200

    "Updated Home function to accept searchParams, added pagination logic and passed products and pagination props to ProductGrid and Pagination components"

--- my-next-app/app/page.js
+++ my-next-app/app/page.js
@@ -3,11 +3,23 @@ import { fetchProducts } from '@/lib/api'
 import ProductGrid from '@/components/ProductGrid'
 import Pagination from '@/components/Pagination'
 
-export default function Home() {
+export default async function HomePage({searchParams}) {
+  const currentPage = searchParams.page ? parseInt(searchParams.page, 10) : 1
+  const productsPerPage = 20
+  const skip = (currentPage - 1) * productsPerPage
+  
+  // Fetch products with skip
+  const { products, total } = await fetchProducts(productsPerPage, skip)
+
   return (
+
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">Our Products</h1>
-      <ProductGrid />
+      <ProductGrid  products={products}/>
+      <Pagination 
+        currentPage={currentPage}
+        totalProducts={total}
+        productsPerPage={productsPerPage}/>
     </div>
   )
 }
